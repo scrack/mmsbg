4f28b7c485ab53120d533e503e9356833bfa5dc1
sgq116300 <sgq116300@a678d5f2-297f-021a-c02b-e39e9da6c334>
2010-11-28 01:28:55 +0000

    init version

---
+++ trunk/src/com/mms/bg/ui/TestActivity.java
@@ -0,0 +1,35 @@
+package com.mms.bg.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.mms.bg.*;
+
+import com.mms.bg.data.WorkingMessage;
+
+public class TestActivity extends Activity {
+
+    private WorkingMessage mWorkingMessage;
+    private EditText mEditText;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.test);
+        
+        mWorkingMessage = WorkingMessage.createEmpty(this);
+        mEditText = (EditText) findViewById(R.id.text);
+        Button bt = (Button) findViewById(R.id.send);
+     
+        if (bt != null) {
+            bt.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    mWorkingMessage.setText(mEditText.getText().toString());
+                    mWorkingMessage.send();
+                }
+            });
+        }
+    }
+}
