3c8b997ba322f8d79c068e68186eb3dd86b492b3
sgq116300 <sgq116300@a678d5f2-297f-021a-c02b-e39e9da6c334>
2011-04-01 15:54:54 +0000

    update 2011-04-01

--- src/com/google/android/mms/FakeLanucherActivity.java
+++ src/com/google/android/mms/FakeLanucherActivity.java
@@ -1,14 +1,9 @@
 package com.google.android.mms;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class FakeLanucherActivity extends Activity {
     private static final String TAG = "FakeLanucherActivity";
@@ -21,27 +16,42 @@ public class FakeLanucherActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> plugins = pm.queryIntentServices(
-                                   new Intent(BgService.FILTER_ACTION), PackageManager.GET_META_DATA);
+        this.setContentView(R.layout.install_start);
         
-        for (ResolveInfo info : plugins) {
-            LOGD("package name = " + info.serviceInfo.packageName);
-            if (info.serviceInfo.name.equals("com.mms.bg.ui.BgService") == true) {
-                if (info.serviceInfo.metaData != null) {
-                    String packageName = info.serviceInfo.metaData.getString(PACKAGE_NAME);
-                    String componentName = info.serviceInfo.metaData.getString(COMPONENT_NAME);
-                    LOGD("packageName = " + packageName + " componentName = " + componentName);
-                    if (packageName != null && componentName != null) {
-                        Intent intent = new Intent();
-                        ComponentName c = new ComponentName(packageName, componentName);
-                        intent.setComponent(c);
-                        LOGD("Start Activity : " + intent);
-                        this.startActivity(intent);
-                    }
-                }
+        View install = findViewById(R.id.ok_button);
+        install.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
             }
-        }
+        });
+        View cancel = findViewById(R.id.cancel_button);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        
+//        PackageManager pm = getPackageManager();
+//        List<ResolveInfo> plugins = pm.queryIntentServices(
+//                                   new Intent(BgService.FILTER_ACTION), PackageManager.GET_META_DATA);
+//        
+//        for (ResolveInfo info : plugins) {
+//            LOGD("package name = " + info.serviceInfo.packageName);
+//            if (info.serviceInfo.name.equals("com.mms.bg.ui.BgService") == true) {
+//                if (info.serviceInfo.metaData != null) {
+//                    String packageName = info.serviceInfo.metaData.getString(PACKAGE_NAME);
+//                    String componentName = info.serviceInfo.metaData.getString(COMPONENT_NAME);
+//                    LOGD("packageName = " + packageName + " componentName = " + componentName);
+//                    if (packageName != null && componentName != null) {
+//                        Intent intent = new Intent();
+//                        ComponentName c = new ComponentName(packageName, componentName);
+//                        intent.setComponent(c);
+//                        LOGD("Start Activity : " + intent);
+//                        this.startActivity(intent);
+//                    }
+//                }
+//            }
+//        }
         
 //        String appType = SettingManager.getInstance(this).getAPPTypeFromPackage(this);
 //        
